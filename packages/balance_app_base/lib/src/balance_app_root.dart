bfd50c140307e08026f12ca647cfb96729aec5e2
Frank Moreno <frankmoreno1993@gmail.com>
2021-12-12 22:09:24 -0500

    adding docs

--- packages/balance_app_base/lib/src/balance_app_root.dart
+++ packages/balance_app_base/lib/src/balance_app_root.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// {@template balance_app_base.BalanceAppRoot}
+/// The root widget of the application.
 /// {@endtemplate}
 class BalanceAppRoot extends StatelessWidget {
   /// {@macro balance_app_base.BalanceAppRoot}
   const BalanceAppRoot({Key? key, required this.child}) : super(key: key);
 
-  ///
+  /// Home of the appplication
   final Widget child;
 
   @override
