b468cac5875989374b524836624485bc31103e56
Frank Moreno <frankmoreno1993@gmail.com>
2021-12-13 22:04:58 -0500

    formating generated template

--- lib/config/config_generator.sh
+++ lib/config/config_generator.sh
@@ -5,12 +5,10 @@
 # bash config_generator.sh > config_staging.dart
 # bash config_generator.sh > config_production.dart
 
-echo "
-// ignore_for_file: lines_longer_than_80_chars
+echo "// ignore_for_file: lines_longer_than_80_chars
 import 'package:nubank_flutter_challenge/config/config.dart';
 
 const config = AppConfig(
   serverUrl: '$SERVE_URL',
   token: '$TOKEN',
-);
-"
+);"
