3100063f09b25c23ca7fdee97a356d40b6f81aaf
Frank Moreno <frankmoreno1993@gmail.com>
2021-12-14 07:07:21 -0500

    organizing utils

---
+++ lib/utils/option_async_value.dart
@@ -0,0 +1,11 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:oxidized/oxidized.dart';
+
+extension OptionAsyncValueFlattener<T> on Option<AsyncValue<T>> {
+  AsyncValue<T> flat() {
+    return when(
+      some: (value) => value,
+      none: () => const AsyncLoading(),
+    );
+  }
+}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,1 @@
 export 'option_async_value.dart';
-
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:oxidized/oxidized.dart';
-
-extension OptionAsyncValueFlattener<T> on Option<AsyncValue<T>> {
-  AsyncValue<T> flat() {
-    return when(
-      some: (value) => value,
-      none: () => const AsyncLoading(),
-    );
-  }
-}
