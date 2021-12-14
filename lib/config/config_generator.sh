09f8c2ea500e612910f5fa4708a459c3ea1c45b1
Frank Moreno <frankmoreno1993@gmail.com>
2021-12-13 22:00:53 -0500

    generator only make echo

--- lib/config/config_generator.sh
+++ lib/config/config_generator.sh
@@ -5,7 +5,7 @@
 # bash config_generator.sh > config_staging.dart
 # bash config_generator.sh > config_production.dart
 
-file="
+echo "
 // ignore_for_file: lines_longer_than_80_chars
 import 'package:nubank_flutter_challenge/config/config.dart';
 
@@ -14,7 +14,3 @@ const config = AppConfig(
   token: '$TOKEN',
 );
 "
-
-echo $file > config_development.dart
-echo $file > config_staging.dart
-echo $file > config_production.dart
