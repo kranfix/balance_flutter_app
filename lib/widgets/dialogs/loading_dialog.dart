1af72bc17dadd50607e656a085238859a8cd602d
Frank Moreno <frankmoreno1993@gmail.com>
2021-12-15 02:44:23 -0500

    fix uneeded import

--- lib/widgets/dialogs/loading_dialog.dart
+++ lib/widgets/dialogs/loading_dialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:nubank_flutter_challenge/utils/utils.dart';
+//import 'package:nubank_flutter_challenge/utils/utils.dart';
 import 'package:nubank_flutter_challenge/widgets/commons/commons.dart';
 
 class LoadingDialog extends StatelessWidget {
